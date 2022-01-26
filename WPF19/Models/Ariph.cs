30be2f564987c8bbbf5763a677fc489cd3cc0eca
milov <milov@DESKTOP-M98P6EU>
2022-01-26 19:50:32 +0300

    Добавьте файлы проекта.

---
+++ WPF19/Models/Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF19.Models
+{
+    class Ariph
+    {
+        public static double Calc(int r) => (2 * Math.PI * r);
+    }
+}
---
+++ WPF19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPF19.Models;
+
+namespace WPF19.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName]string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private int radius;
+        public int Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+        private double circumference;
+        public double Circumference
+        {
+            get => circumference;
+            set
+            {
+                circumference = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Circumference = Ariph.Calc(Radius);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            if(Radius!= 0) return true; 
+            else return false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+
+}
